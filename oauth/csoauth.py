664029c54817826e77b2c36d5b66c1387468f07c
Kshitij <kshitijshukla345@gmail.com>
2025-03-19 20:32:29 +0530

    new version that hides client secrets on input as well as it checks for input combined ids and skips any non standard combined ids.

--- oauth/csoauth.py
+++ oauth/csoauth.py
@@ -1,16 +1,19 @@
 import requests
-def get_bearer(client_id, client_secret, member_cid):
-    client_id = client_id
-    client_secret = client_secret
-    member_cid = member_cid
+
+def get_bearer(client_id, client_secret, member_cid=None):
     token_url = "https://api.eu-1.crowdstrike.com/oauth2/token"
     token_headers = {
         "accept": "application/json",
         "Content-Type": "application/x-www-form-urlencoded"
     }
-    if "09a068" in member_cid:
-        token_response = requests.post(token_url, headers=token_headers, data={"client_id": client_id,"client_secret": client_secret})
-    else:
-        token_response = requests.post(token_url, headers=token_headers, data={"client_id": client_id,"client_secret": client_secret,"member_cid": member_cid})
+    data = {
+        "client_id": client_id,
+        "client_secret": client_secret
+    }
+    
+    if member_cid and "09a068" not in member_cid:
+        data["member_cid"] = member_cid
+
+    token_response = requests.post(token_url, headers=token_headers, data=data)
     bearer_token = token_response.json().get("access_token")
     return bearer_token
